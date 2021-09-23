6e2b52a83d6cdf598c578a7bffe1be23a9eef223
akasifislam <aseasifislam@gmail.com>
2021-09-23 23:33:55 +0600

    feat: shop page mastering

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('/', HomeComponent::class);
+
+
+Route::get('/shop', ShopComponent::class);
