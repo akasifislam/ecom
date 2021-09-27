c8c0ba73a872a7c70a749978a879be9fa9fc3de8
akasifislam <aseasifislam@gmail.com>
2021-09-27 23:08:58 +0600

    test

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -43,9 +43,7 @@
 							</ul>
 						</div>
 						<div class="topbar-menu right-menu">
-							<ul>
-								<li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-								<li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>								
+							<ul>								
 								<li class="menu-item lang-menu menu-item-has-children parent">
 									<a title="English" href="#"><span class="img label-before"><img src="assets/images/lang-en.png" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
@@ -69,6 +67,30 @@
 										</li>
 									</ul>
 								</li>
+								@if (Route::has('login'))
+									@auth
+										@if (Auth::user()->utype==='ADM')
+										<li class="menu-item menu-item-has-children parent" >
+											<a title="My Account" href="#">My Account({{ Auth::user()->name }})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											<ul class="submenu curency" >
+												<li class="menu-item" >
+													<a title="Pound (GBP)" href="{{ route('dashboard') }}">Dashboard</a>
+												</li>
+												<li class="menu-item" >
+													<a title="Euro (EUR)" href="#">Euro (EUR)</a>
+												</li>
+												<li class="menu-item" >
+													<a title="Dollar (USD)" href="#">Dollar (USD)</a>
+												</li>
+											</ul>
+										</li>
+										@endif
+									
+									@else
+										<li class="menu-item" ><a title="Register or Login" href="{{ route('login') }}">Login</a></li>
+										<li class="menu-item" ><a title="Register or Login" href="{{ route('register') }}">Register</a></li>
+									@endauth
+								@endif
 							</ul>
 						</div>
 					</div>
